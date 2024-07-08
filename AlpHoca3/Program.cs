0ed74cce19201f2baca8d787eb3661397da6a907
Meltem Ceran <meltem.ceran@bilgeadamboost.com>
2024-07-08 17:41:43 +0300

    SwitchCase örnek

---
+++ AlpHoca3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+
+namespace AlpHoca3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SwitchCaseOrnekler();
+
+        }
+
+        private static void SwitchCaseOrnekler()
+        {
+
+            int sayi;
+            Console.WriteLine("Lütfen bir sayı yazınız");
+            sayi = Convert.ToInt32(Console.ReadLine());
+
+            switch (sayi)
+            {
+                case < 0:
+                    Console.WriteLine("Negatif");
+                    break;
+                case 1:
+                    Console.WriteLine("Uno");
+                    break;
+                case 2:
+                    Console.WriteLine("Dos");
+                    break;
+                case 3:
+                    Console.WriteLine("Tres");
+                    break;
+                case 4:
+                    Console.WriteLine("Quatro");
+                    break;
+                case 5:
+                    Console.WriteLine("Cinco");
+                    break;
+                default:
+                    Console.WriteLine("Otro Numero");
+                    break;
+
+            }
+
+            //eğer birden çok sayı kullanacaksam şöyle
+            //case(<0 , < 0) diye yazıyoruz.
+            //ya da case( <0, not 3) sayı1 sıfırdan küçük ve sayı2 3 değil.
+            // ya da case(>-10 and <0 , <0) sayı1 sıfırdan küçük ve -10dan büyük sayı2 sıfırdan küçük
+            // ya da case(>-10 and <0 , <0) when sayi !=88 
+            //"when" switch dışındaki değişkenlerdir.
+        }
+    }
+}
