161bcb49065d208157e4012dec0af0d57afd0fba
Meltem Ceran <meltem.ceran@bilgeadamboost.com>
2024-07-12 17:50:57 +0300

    Örnekler devam ve ilk oyunumuz <3

--- Döngüler/Program.cs
+++ Döngüler/Program.cs
@@ -1,6 +1,8 @@
 ﻿
 
 
+using System.Numerics;
+
 namespace Döngüler
 {
     internal class Program
@@ -18,8 +20,208 @@ namespace Döngüler
             //Görev9();
             //Görev10();
             //Görev11();
-            Görev12();
+            //Görev12();
+            //Görev13();
+            //Görev14();
+            //Görev15();
+            //Görev16();
+            //Görev17();
+            MinikBirOyun();
+            
 
+        }
+
+        private static void MinikBirOyun()
+        {
+            int sayi, tutulanSayi, denemeSayisi=0;
+            bool kazadiMi = false;
+            tutulanSayi = new Random().Next(1, 21);
+
+            Console.WriteLine("Sayı Tahmini Oyunu! Hoşgeldiniz:)");
+            Console.WriteLine("10 tane tahmin hakkınız vardır.");
+
+            for (int tahminAdimi = 1; tahminAdimi <= 10; tahminAdimi++)
+            {
+
+                denemeSayisi++;
+                Console.WriteLine($"{tahminAdimi} . tahmin hakkınızı yazınız:");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                while (sayi < 1 || sayi > 20)
+                {
+                    Console.WriteLine("Lütfen 1-20 arasında bir değer giriniz:");
+                    sayi = Convert.ToInt32(Console.ReadLine()) ;
+                }
+                if(sayi==tutulanSayi)
+                {
+                    kazadiMi=true;
+                    Console.WriteLine($"Tebrikler {denemeSayisi} . denemede bildiniz!");
+                    break;
+                }
+                else if(sayi < tutulanSayi)
+                {
+                    Console.WriteLine("Yukarı :)");
+                }
+                else
+                    Console.WriteLine("Aşağı :)");
+            }
+            if(!kazadiMi)
+                Console.WriteLine($"Kaybettiniz :( Sayı {tutulanSayi} idi.");
+
+
+
+
+
+        }   
+
+
+
+
+        private static void Görev17()
+        {
+            //Klavyeden girilen 10 adet sayıdan (int) 3 basamaklı olanları do-while ile ekrana yazdırınız.
+
+            int sayi;
+            int sayac = 1;
+
+
+            do
+            {
+                Console.WriteLine($"Lütfen {sayac} .  sayıyı giriniz:");
+                sayi = Convert.ToInt32(Console.ReadLine());
+
+                if ((sayi >= 100 && sayi <= 999) || (sayi >= -999 && sayi <= -100))
+                {
+                    Console.WriteLine(sayi);
+                }
+
+                sayac++;
+            } while (sayac <= 10);
+
+
+
+
+
+
+        }
+
+        private static void Görev16()
+        {
+            //Klavyeden bir kelime istenmelidir. Bu kelime ANK18 olunca program sonlanmalıdır. Bunu do-while ile yazınız.
+
+
+
+
+            string metin;
+            do
+            {
+                Console.WriteLine("Dilediğiniz bir cümle giriniz:");
+
+                metin = Console.ReadLine();
+
+            } while (metin != "ANK18");
+        }
+
+        private static void Görev15()
+        {
+            //Klavyeden girilen 10 adet sayıdan sadece 3'e, sadece 5'e ve ikisinede tam bölünenlerin adetleri.
+
+
+            int sayi;
+            int i = 0;
+            int sayac3 = 0;
+            int sayac5 = 0;
+            int sayac35 = 0;
+
+            while (i < 10)
+            {
+                Console.WriteLine($"Lütfen {i + 1} . sayıyı giriniz:");
+                sayi = Convert.ToInt32(Console.ReadLine());
+                i++;
+
+                if (sayi % 3 == 0 && sayi % 5 != 0)
+                {
+                    sayac3++;
+                }
+                else if (sayi % 5 == 0 && sayi % 3 != 0)
+                {
+                    sayac5++;
+                }
+                else if (sayi % 3 == 0 && sayi % 5 == 0)
+                {
+                    sayac35++;
+                }
+
+
+            }
+            Console.WriteLine($"3'e bölünenler: {sayac3} , 5'e bölünenler: {sayac5} , 3'e ve 5'e bölünenler: {sayac35}");
+        }
+
+        private static void Görev14()
+        {
+            //Klavyeden girilen iki sayının çarpımını * kullanmadan while ve + ile yapınız.
+
+            Console.WriteLine($"Lütfen bir sayı giriniz:");
+
+            uint s1 = Convert.ToUInt32(Console.ReadLine());
+
+            Console.WriteLine($"Lütfen bir sayı giriniz:");//3
+
+            uint s2 = Convert.ToUInt32(Console.ReadLine());   //5
+
+            uint sayac = 1;
+
+            uint sonuc = 0;
+
+            while (sayac <= s1)
+            {
+                sonuc += s2;
+                sayac++;
+            }
+            Console.WriteLine($"Sonuç: {sonuc}");
+
+
+        }
+
+        private static void Görev13()
+        {
+            //Klavyeden girilen iki sayı arasındski sayıların toplamını while ile.
+
+            Console.WriteLine("Lütfen iki sayı giriniz:");
+
+            int sayi1 = Convert.ToInt32(Console.ReadLine());
+
+            int sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            int toplam = 0;
+
+            if (sayi1 < sayi2)
+            {
+
+                while (sayi1 <= sayi2)
+                {
+                    toplam += sayi1;
+                    sayi1 += 1;
+
+                }
+                Console.WriteLine($"Sonuç: {toplam}");
+            }
+            else if (sayi2 < sayi1)
+            {
+
+                while (sayi2 <= sayi1)
+                {
+                    toplam += sayi2;
+                    sayi2 += 1;
+
+                }
+                Console.WriteLine($"Sonuç: {toplam}");
+            }
+            else
+            {
+                Console.WriteLine("Lütfen birbirine eşit olmayan iki sayı giriniz.");
+            }
+
+            //Bunu yedekleyerek yapabiliriz. sayıların yerini değiştirmek için.
         }
 
         private static void Görev12()
@@ -63,7 +265,7 @@ namespace Döngüler
             do
             {
                 toplam = toplam * i;
-                      i++;
+                i++;
             } while (i <= sayi);
             Console.WriteLine($"Sonuç: {toplam}");
 
@@ -73,7 +275,7 @@ namespace Döngüler
         {
             int sayi = 1;
             int toplam = 0;
-            
+
             while (sayi <= 100)
             {
                 toplam = toplam + sayi;
@@ -108,7 +310,7 @@ namespace Döngüler
             while (sayi <= 10)
             {
                 Console.WriteLine($" 7 * {sayi} = {sayi * 7}");
-                    sayi++;
+                sayi++;
             }
         }
 
@@ -116,9 +318,9 @@ namespace Döngüler
         {
             for (int i = 1; i <= 10; i++)
             {
-                for(int j = 1; j <= 10; j++) 
+                for (int j = 1; j <= 10; j++)
                 {
-                    Console.WriteLine($"{i} * {j} = {i*j}");
+                    Console.WriteLine($"{i} * {j} = {i * j}");
                 }
 
                 Console.WriteLine("----------");
@@ -353,5 +555,6 @@ namespace Döngüler
             } while (j < 10);
 
         }
+        
     }
 }
